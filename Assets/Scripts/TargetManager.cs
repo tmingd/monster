ec78fb211cfe3b8c13e1891e21d016ad813d45b9
tmingda <1787395636@qq.com>
2023-06-09 12:32:27 +0800

    first

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour {
+
+    //保存所有该目标下的怪物
+    public GameObject[] monsters;
+    //保存目前处于激活状态的怪物
+    public GameObject activeMonster = null;
+    //表示目标所在的位置（0-8）
+    public int targetPosition;
+
+    private void Start()
+    {
+        foreach(GameObject monster in monsters)
+        {
+            monster.GetComponent<BoxCollider>().enabled = false;
+            monster.SetActive(false);
+        }
+        //调用协程
+        StartCoroutine("AliveTimer");
+    }
+
+    //随机激活怪物
+    private void ActivateMonster()
+    {
+        int index = Random.Range(0, monsters.Length);
+        activeMonster = monsters[index];
+        activeMonster.SetActive(true);
+        activeMonster.GetComponent<BoxCollider>().enabled = true;
+        //调用死亡时间的协程
+        StartCoroutine("DeathTimer");
+    }
+
+    //迭代器方法，设置生成的等待时间
+    IEnumerator AliveTimer()
+    {
+        //等待1-4秒后执行ActivateMonster方法
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        ActivateMonster();
+    }
+
+    //使激活状态的怪物变为未激活状态
+    private void DeActivateMonster()
+    {
+        if (activeMonster != null)
+        {
+            activeMonster.GetComponent<BoxCollider>().enabled = false;
+            activeMonster.SetActive(false);
+            activeMonster = null;
+        }
+        //调用激活时间的协程，达到一个反复激活和死亡的循环
+        StartCoroutine("AliveTimer");
+    }
+
+    //迭代器，设置死亡的等待时间
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(Random.Range(3, 8));
+        DeActivateMonster();
+    }
+
+    //更新生命周期。当子弹击中怪物时，或者当重新开始游戏时
+    //停止所有的协程
+    //将当前处于激活状态的怪物变为未激活状态，清空activeMonster
+    //重新开始AliveTimer的协程（随机激活怪物）
+    public void UpdateMonsters()
+    {
+        StopAllCoroutines();
+        if (activeMonster != null)
+        {
+            activeMonster.GetComponent<BoxCollider>().enabled = false;
+            activeMonster.SetActive(false);
+            activeMonster = null;         
+        }
+        StartCoroutine("AliveTimer");
+    }
+
+    //按照给定的怪物类型激活怪物
+    //停止所有协程
+    //将当前激活状态的怪物（如果有的话）转变为未激活状态
+    //激活给定类型的怪物
+    //调用死亡时间的协程
+    public void ActivateMonsterByType(int type)
+    {
+        StopAllCoroutines();
+        if (activeMonster != null)
+        {
+            activeMonster.GetComponent<BoxCollider>().enabled = false;
+            activeMonster.SetActive(false);
+            activeMonster = null;
+        }
+        activeMonster = monsters[type];
+        activeMonster.SetActive(true);
+        activeMonster.GetComponent<BoxCollider>().enabled = true;
+        StartCoroutine("DeathTimer");
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public static UIManager _instance;
+    //获得2个Text组件
+    public Text shootNumText;
+    public Text scoreText;
+
+    public int shootNum = 0;
+    public int score = 0;
+    //音乐开关的单选框和播放背景音乐的AudioSource
+    public Toggle musicToggle;
+    public AudioSource musicAudio;
+
+    public Text messageText;
+
+    private void Awake()
+    {
+        _instance = this;
+        if(PlayerPrefs.HasKey("MusicOn"))
+        {
+            if(PlayerPrefs.GetInt("MusicOn") == 1)
+            {
+                musicToggle.isOn = true;
+                musicAudio.enabled = true;
+            }
+            else
+            {
+                musicToggle.isOn = false;
+                musicAudio.enabled = false;
+            }
+        }
+        else
+        {
+            musicToggle.isOn = true;
+            musicAudio.enabled = true;
+        }
+    }
+
+    private void Update()
+    {
+        //更新Text组件的显示内容
+        shootNumText.text = shootNum.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void MusicSwitch()
+    {
+        //通过判断单选框是否被勾选上，从而来决定是否播放背景音乐
+        if(musicToggle.isOn == false)
+        {
+            musicAudio.enabled = false;
+            //保存音乐开关的状态，0代表关闭状态
+            PlayerPrefs.SetInt("MusicOn", 0);
+        }
+        else
+        {
+            musicAudio.enabled = true;
+            PlayerPrefs.SetInt("MusicOn", 1);
+        }
+        PlayerPrefs.Save();
+    }
+
+    //增加射击数（当开枪时）
+    public void AddShootNum()
+    {
+        shootNum += 1;
+    }
+    //增加得分（当射中怪物时）
+    public void AddScore()
+    {
+        score += 1;
+    }
+
+    public void ShowMessage(string str)
+    {
+        messageText.text = str;
+    }
+     
+}
